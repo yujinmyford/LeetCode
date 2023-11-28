1e7f8f17690e4b57d286669212d2b377cc2e1a64
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2023-11-27 21:40:18 -0500

    Create (20)Valid_Parentheses.py

---
+++ (20)Valid_Parentheses.py
@@ -0,0 +1,34 @@
+# Stack, Hashmap
+# Runtime: O(n)
+# Space: O(n)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Hashmap to keep track of open and close brackets
+        match = {"(": ")", "{": "}", "[": "]"}
+        # Stack to use
+        stack = []
+
+        # Iterate through s
+        for cur in s:
+
+            # Edge case where there is only closing bracket
+            # Stack is empty and adding closing bracket
+            if not stack and cur not in match:
+                return False
+            # If cur is in match, means opening bracket, append to stack then continue
+            if cur in match:
+                stack.append(cur)
+                continue
+            # If cur matches opening bracket of top of stack, valid bracket. Pop then continue
+            if match[stack[-1]] == cur:
+                stack.pop()
+                continue
+            # If cur does not correspond to opening bracket, invalid parentheses
+            else:
+                return False
+        
+        # After iterating through stack, if stack is empy, return true. If not, false
+        if not stack:
+            return True
+        else:
+            return False
