b6c57ee72b4599eb0dd83e37e3d63e4d99f1c340
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-03-20 22:00:40 -0400

    Create 0033. Search In Rotated Sorted Array.py

---
+++ 0033. Search In Rotated Sorted Array.py
@@ -0,0 +1,22 @@
+# Runtime: O(log n)
+# Space: O(1)
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while(low <= high):
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[low] > nums[mid]:
+                if target >= nums[low] or target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+        return -1
