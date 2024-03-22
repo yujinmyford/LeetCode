808075fdfb9eddd14c4eff910b8abb90e87d9353
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-03-22 13:04:17 -0400

    Create 0074. Search a 2D Matrix.py

---
+++ 0074. Search a 2D Matrix.py
@@ -0,0 +1,38 @@
+# Runtime: O(log m + log n)
+# Space: O(1)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        top = 0
+        bot = rows - 1
+
+        # Binary Search for rows, to find which row target will be in
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        
+        if not (top <= bot):
+            return False
+        
+        row = (top + bot) // 2
+        left = 0
+        right = cols - 1
+
+        # Binary Search within the row, to find target
+        while left <= right:
+            mid = (left + right) // 2
+            if target > matrix[row][mid]:
+                left = mid + 1
+            elif target < matrix[row][mid]:
+                right = mid - 1
+            else:
+                return True
+            
+        return False
