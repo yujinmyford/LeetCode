143c923be99993772708d024f9ab5a2d2c503b2d
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-07-16 21:08:38 -0700

    Update 0001. Two Sum.py

--- 0001. Two Sum.py
+++ 0001. Two Sum.py
@@ -1,10 +1,27 @@
+
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         finalArray = []
+#         for i, x in enumerate(nums):
+#             for j, y in enumerate(nums):
+#                 if (i == j): break
+#                 if (x + y == target):
+#                     finalArray.append(j)
+#                     finalArray.append(i)
+#                     return finalArray
+
+# 7/16
+# Runtime: O(n)
+# Space: O(1)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        finalArray = []
-        for i, x in enumerate(nums):
-            for j, y in enumerate(nums):
-                if (i == j): break
-                if (x + y == target):
-                    finalArray.append(j)
-                    finalArray.append(i)
-                    return finalArray
+        nums_hash = {}
+        for i in range(len(nums)):
+            nums_hash[nums[i]] = i
+        
+        cur_tar = 0
+        for i in range(len(nums)):
+            cur_tar = target - nums[i]
+            if (cur_tar in nums_hash) and (nums_hash[cur_tar] != i):
+                return [i, nums_hash[cur_tar]]
