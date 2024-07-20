bad028ef8fbfbed0bfc6b44255cff319cc48afd4
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-07-19 17:40:09 -0700

    Update 0094. Binary Tree Inorder Traversal.py

--- 0094. Binary Tree Inorder Traversal.py
+++ 0094. Binary Tree Inorder Traversal.py
@@ -6,18 +6,20 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # Iterative
-        res, stack = [], []
-        cur = root
 
-        while cur or stack:
-            while cur:
-                stack.append(cur)
-                cur = cur.left
-            cur = stack.pop()
-            res.append(cur.val)
-            cur = cur.right
-        return res
+        
+        # # Iterative
+        # res, stack = [], []
+        # cur = root
+
+        # while cur or stack:
+        #     while cur:
+        #         stack.append(cur)
+        #         cur = cur.left
+        #     cur = stack.pop()
+        #     res.append(cur.val)
+        #     cur = cur.right
+        # return res
 
         # Recursive
         res = []
