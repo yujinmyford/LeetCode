ae262dd87703dd8d7394bd095a214d789618ede2
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-07-25 20:36:09 -0700

    Update 0217. Contains Duplicate.py

--- 0217. Contains Duplicate.py
+++ 0217. Contains Duplicate.py
@@ -9,3 +9,23 @@ class Solution:
             containsList.add(num)
         
         return False
+
+
+
+
+# # 7/25
+# # Runtime: O(n)
+# # Space: O(n)
+# class Solution:
+#     def hasDuplicate(self, nums: List[int]) -> bool:
+
+#         exists = {}
+
+#         for i in nums:
+#             if i in exists:
+#                 return True
+#             else:
+#                 exists[i] = 1
+        
+
+#         return False
