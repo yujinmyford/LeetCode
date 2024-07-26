e6c70a8cdd1095b9c94faa34661ad1ddc9363ae0
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-07-25 20:46:34 -0700

    Update 0242. Valid Anagram.py

--- 0242. Valid Anagram.py
+++ 0242. Valid Anagram.py
@@ -14,3 +14,33 @@ class Solution:
         if len(listT) == 0:
             return True
         return False
+
+
+
+
+# # 7/25
+# # Hashmap
+# # Runtime: O(n)
+# # Space: O(s+t) = O(n)
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+
+#         s_hash = {}
+#         t_hash = {}
+
+#         for i in s:
+#             if i in s_hash:
+#                 s_hash[i] += 1
+#             else:
+#                 s_hash[i] = 1
+        
+
+#         for i in t:
+#             if i in t_hash:
+#                 t_hash[i] += 1
+#             else:
+#                 t_hash[i] = 1
+        
+
+#         return s_hash == t_hash
