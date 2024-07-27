91fd90fcc38d38c5e60440df6bfcc4429bf99b1f
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-07-27 13:59:18 -0700

    Update 0015. 3Sum.py

--- 0015. 3Sum.py
+++ 0015. 3Sum.py
@@ -32,3 +32,36 @@ class Solution:
                         left += 1
             
         return triplets
+
+
+# # 7/27
+
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+#         triplets = []
+#         nums.sort()
+
+#         for i in range(len(nums)):
+
+#             if i > 0 and nums[i] == nums[i - 1]:
+#                 continue
+            
+#             l = i + 1
+#             r = len(nums) - 1
+
+#             while l < r:
+#                 curSum = nums[i] + nums[l] + nums[r]
+
+#                 if curSum > 0:
+#                     r -= 1
+#                 elif curSum < 0:
+#                     l += 1
+#                 else:
+#                     triplets.append([nums[i], nums[l], nums[r]])
+#                     l += 1
+#                     r -= 1
+#                     while nums[l] == nums[l - 1] and l < r:
+#                         l += 1
+
+#         return triplets
