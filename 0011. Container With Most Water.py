5d1652d0c23fd48a41d36e29a10970d357d6b914
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-07-27 14:21:47 -0700

    Update 0011. Container With Most Water.py

--- 0011. Container With Most Water.py
+++ 0011. Container With Most Water.py
@@ -4,21 +4,49 @@
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # Instantiate left, right, and max_area
         left = 0
         right = len(height) - 1
         max_area = 0
-    
+
         while left < right:
-            # If left is smaller, use left to calculate the area since it is smaller, then update max_area and increment left
             if height[left] < height[right]:
                 cur_area = height[left] * (right - left)
                 max_area = max(max_area, cur_area)
                 left += 1
-            # If right is smaller, use right to calculate the area since it is smaller, then update max_area and decrement right
             else:
                 cur_area = height[right] * (right - left)
                 max_area = max(max_area, cur_area)
                 right -= 1
         return max_area
+
+# # Neetcode
+
+# # Brute Force, calculates every single combination possible
+# # Time complexity = O(n^2)
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         res = 0
+
+#         for l in range(len(hieght)):
+#             for r in range(l + 1, len(height)):
+#                 area = (r - l) * min(height[l], height[r])
+#                 res = max(res, area)
+#         return res
+
+# # Two pointer
+# # Time complexity = O(n)
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         l, r = 0, len(height) - 1
+#         res = 0
+
+#         while l < r:
+#             res = max(res, min(height[l], height[r]) * (r - l))
+#             # Move pointer for smaller side
+#             if height[l] < height[r]:
+#                 l += 1
+#             elif height[r] <= height[l]:
+#                 r -= 1
+            
+#         return res
 
