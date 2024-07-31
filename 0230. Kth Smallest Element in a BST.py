90fde581a2e1b841099a9258f5ba7ce6699593f1
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-07-31 04:26:51 -0700

    Update 0230. Kth Smallest Element in a BST.py

--- 0230. Kth Smallest Element in a BST.py
+++ 0230. Kth Smallest Element in a BST.py
@@ -4,17 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# BST, DFS(inorder)
+# Runtime: O(n)
+# Space: O(n)
+
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
 
         res = []
 
-        def inorder(root, res):
-            if root is None:
-                return
-            inorder(root.left, res)
-            res.append(root.val)
-            inorder(root.right, res)
-
-        inorder(root, res)
+        self.inorder(root, res)
         return res[k - 1]
+
+    def inorder(self, root, res):
+        if root is None:
+            return
+        self.inorder(root.left, res)
+        res.append(root.val)
+        self.inorder(root.right, res)
