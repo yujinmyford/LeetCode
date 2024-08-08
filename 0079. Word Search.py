192c0b6fec3a1de5412849869cdce32fea6b94ba
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-08-07 22:21:09 -0700

    Create 0079. Word Search.py

---
+++ 0079. Word Search.py
@@ -0,0 +1,30 @@
+# Backtracking, DFS
+# Runtime: O(n * m)
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        row, col = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or r >= row or c >= col or word[i] != board[r][c] or (r, c) in path):
+                return False
+            
+            path.add((r,c))
+
+            res = (dfs(r + 1, c, i + 1) or 
+                    dfs(r - 1, c, i + 1) or 
+                    dfs(r, c + 1, i + 1) or 
+                    dfs(r, c - 1, i + 1))
+            
+            path.remove((r, c))
+            return res
+        
+        for r in range(row):
+            for c in range(col):
+                if dfs(r, c, 0):
+                    return True
+        
+        return False
