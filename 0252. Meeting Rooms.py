90ceaff65078638ac60cb6158e024a4223014f89
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-08-14 22:22:43 -0700

    Create 0252. Meeting Rooms.py

---
+++ 0252. Meeting Rooms.py
@@ -0,0 +1,24 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+# Intervals
+# Runtime: O(n)
+# Space: O(1)
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda i: i.start)
+
+        for i in range(1, len(intervals)):
+            i1 = intervals[i - 1]
+            i2 = intervals[i]
+
+            if i1.end > i2.start:
+                return False
+        return True
+ 
