9ea93b262fc2a13cd173541f82250dd68b8ff13f
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-08-21 13:45:30 -0700

    Update 0198. House Robber.py

--- 0198. House Robber.py
+++ 0198. House Robber.py
@@ -1,5 +1,28 @@
 # DP
 # Runtime: O(n)
+# Space: O(n)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+
+        dp = [0 for i in range(len(nums))]
+
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        
+
+        return dp[len(nums) - 1]
+
+# DP
+# Runtime: O(n)
 # Space: O(1)
 
 class Solution:
