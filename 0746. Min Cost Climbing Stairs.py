b0cd6d3bb9ead92b1a363d0bc49927b560ecaf4e
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-08-21 12:59:03 -0700

    Create 0746. Min Cost Climbing Stairs.py

---
+++ 0746. Min Cost Climbing Stairs.py
@@ -0,0 +1,15 @@
+# DP
+# Runtime: O(n)
+# Space: O(n)
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [0 for i in range(len(cost))]
+
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+        
+        return min(dp[len(cost) - 2], dp[len(cost) - 1])
