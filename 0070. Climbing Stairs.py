a79022d9c8d71166d83648c0bfbfee0ce7afb19b
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-08-21 12:52:39 -0700

    Update 0070. Climbing Stairs.py

--- 0070. Climbing Stairs.py
+++ 0070. Climbing Stairs.py
@@ -1,3 +1,19 @@
+# 8/21
+# DP
+# Runtime: O(n)
+# Space: O(n)
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        dp = [0 for _ in range(n + 1)]
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+
+
 # Runtime: O(n)
 # Space: O(1)
 
