07caf173ae0b9e183a779dc25bbac213eba8ccc4
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-09-19 20:57:31 -0700

    Create 0684. Redundant Connection.py

---
+++ 0684. Redundant Connection.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findRedundantConnection(self, edges):
+        graph = collections.defaultdict(set)
+
+        def dfs(source, target):
+            if source not in seen:
+                seen.add(source)
+                if source == target: 
+                    return True
+                for nei in graph[source]:
+                    if dfs(nei, target):
+                        return True
+                return False
+
+        for u, v in edges:
+            seen = set()
+            if u in graph and v in graph and dfs(u, v):
+                return u, v
+            graph[u].add(v)
+            graph[v].add(u)
