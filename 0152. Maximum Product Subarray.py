466b94408edc01256b2b091d96adab32bd044094
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-09-20 22:30:32 -0700

    Update 0152. Maximum Product Subarray.py

--- 0152. Maximum Product Subarray.py
+++ 0152. Maximum Product Subarray.py
@@ -1,3 +1,24 @@
+class Solution:
+    def maxProduct(self, nums):
+        if len(nums) == 0:
+            return 0
+
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+
+        for i in range(1, len(nums)):
+            curr = nums[i]
+            temp_max = max(curr, max(max_so_far * curr, min_so_far * curr))
+            min_so_far = min(curr, min(max_so_far * curr, min_so_far * curr))
+
+            # Update max_so_far after updates to min_so_far to avoid overwriting it
+            max_so_far = temp_max
+            # Update the result with the maximum product found so far
+            result = max(max_so_far, result)
+
+        return result
+
 # DP
 # Runtime: O(n)
 # Space: O(1)
