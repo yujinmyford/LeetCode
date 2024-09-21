03649e0f5b5bc3e7ee5ac340b30b284e0f542fd5
yujinmyford <71200804+yujinmyford@users.noreply.github.com>
2024-09-21 08:25:33 -0700

    Update 0062. Unique Paths.py

--- 0062. Unique Paths.py
+++ 0062. Unique Paths.py
@@ -1,3 +1,27 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        visit = set()
+        ROWS, COLS = m, n
+        if (min(r, c) < 0 or
+            r == ROWS or c == COLS or
+            (r, c) in visit):
+            return 0
+        if r == ROWS - 1 and c == COLS - 1:
+            return 1
+
+        visit.add((r, c))
+
+        count = 0
+        count += self.uniquePaths(r + 1, c)
+        count += self.uniquePaths(r - 1, c)
+        count += self.uniquePaths(r, c + 1)
+        count += self.uniquePaths(r, c - 1)
+
+        visit.remove((r, c))
+        return count
+
+
+
 # DP, bottom up
 # Runtime: O(m*n)
 # Space: O(n)
